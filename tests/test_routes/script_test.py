28ab8e8f7fd8855eb8a0c90d26e12728f14a3604
semen603089 <semen603089@mail.ru>
2023-03-12 16:59:20 +0300

    review

--- tests/test_routes/script_test.py
+++ tests/test_routes/script_test.py
@@ -5,8 +5,8 @@ def test_script_execute(client):
     action = "test_script"
     url=f"/{action}"
     body = {
-        "repo_url": 'surname',
-        "commit_hash": 'union_number'
+        "repo_url": 'https://...',
+        "git_ref": '123123123'
     }
     res = client.post(url, data=json.dumps(body))
     print(str(res.json()))
