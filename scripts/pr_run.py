1bc6d90d53163981da45b895308f8bcc701cdf5e
Grigoriev Semyon <33061489+grigoriev-semyon@users.noreply.github.com>
2023-03-12 17:17:48 +0300

    logging (#9)

--- scripts/pr_run.py
+++ scripts/pr_run.py
@@ -15,10 +15,16 @@ if __name__ == "__main__":
     org, repo = args.repo_url.remove_prefix('https://github.com/').rsplit('/', 1)
     pr_num = re.match("^refs/pull/(?P<pr_num>\d+)", args.git_ref).group('pr_num')
     Popen(['docker', 'rm', '-f', f'pkff_dev__{repo}__pr-{pr_num}']).communicate()
-    Popen([
-        'docker', 'run',
-        '--rm', '--detach',
-        '--network', 'web',
-        '--name', f'pkff_dev__{repo}__pr-{pr_num}',
-        f'ghcr.io/{org}/{repo}:pr-{pr_num}'
-    ]).communicate()
+    Popen(
+        [
+            'docker',
+            'run',
+            '--rm',
+            '--detach',
+            '--network',
+            'web',
+            '--name',
+            f'pkff_dev__{repo}__pr-{pr_num}',
+            f'ghcr.io/{org}/{repo}:pr-{pr_num}',
+        ]
+    ).communicate()
--- tests/test_routes/script_test.py
+++ tests/test_routes/script_test.py
@@ -3,11 +3,8 @@ import json
 
 def test_script_execute(client):
     action = "test_script"
-    url=f"/{action}"
-    body = {
-        "repo_url": 'surname',
-        "commit_hash": 'union_number'
-    }
+    url = f"/{action}"
+    body = {"repo_url": 'https://...', "git_ref": '123123123'}
     res = client.post(url, data=json.dumps(body))
     print(str(res.json()))
     assert res.json()['code'] == 0
